be02fe8d017bc739068fa73a417522fb123d212f
Bianca-SAID <saitbianca17@gmail.com>
2025-02-21 16:53:39 -0700

    Another test

---
+++ PS-CODE.py
@@ -0,0 +1,15 @@
+'''
+CPRG-216-B
+Group 7: Assignment: Programming Strategies
+Description:
+'''
+
+print('*' * 50)
+print("Welcome to Movie Booking System")
+print('*' * 50)
+
+print('*' * 50)
+print("Step 1: Select a Movie")
+print('*' * 50)
+
+#comment test test test 
