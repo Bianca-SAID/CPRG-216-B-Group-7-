464e5831a9a96234bab725c38181b78862d20efb
Bianca-SAID <saitbianca17@gmail.com>
2025-02-25 23:07:31 -0700

    Implemented show time selection and corrected spacing

--- PS-CODE.py
+++ PS-CODE.py
@@ -1,10 +1,10 @@
-'''
+"""
 CPRG-216-B
 Group 7: Assignment: Programming Strategies
 Description:
-'''
+"""
 
-#Format the ticket header
+#Format Ticket 
 print('*' * 50)
 print("{:>40}".format("Welcome to Movie Booking System"))
 print('*' * 50)
@@ -12,4 +12,40 @@ print()
 print('*' * 50)
 print("{:>35}".format("Step 1: Select a Movie"))
 print('*' * 50)
+
+print("1. Avengers: Endgame ($12.00)\n2.The Kitchen ($10.00)\n3.Killers of the Flower Moon ($8.00)\n4.Dune:Part Two ($9.50)")
+
+#Implement movie Selection
+movie = input("Your selection (1-4): ")
+match movie:
+    case "1":
+        print("You selected 'Avengers:Endgame' priced at $12.00")
+    case "2":
+        print("You selected ' The Kitchen' priced at $10.00")
+    case "3":
+        print("You selected 'Killers of the Flower Moon ' priced at $8.00")
+    case "4":
+        print("You selected ' Dune: Part Two' priced at $9.50")
+    case _:
+        print('Invalid Selection. Please choose a valid movie number.')
+
 print()
+print('*' * 50)
+print("{:>38}".format("Step 2: Select a show time"))
+print('*' * 50)
+
+#Implement show time selection
+print("1. 1:00 PM\n2. 4:30 PM\n3. 8:00 PM") 
+show = input("Your selection (1-3): ")
+match show:
+    case "1":
+        print("You selected show time '1:00 PM'")    
+    case "2":
+        print("You selected show time '4:30 PM'")    
+    case "3":
+        print("You selected show time '8:00 PM'")
+    case _:
+        print('Invalid Selection. Please choose a valid show time number.')
+
+
+
