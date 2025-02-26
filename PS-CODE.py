69840c7bc0f818672a339b6f70f18735cf027d15
Bianca-SAID <saitbianca17@gmail.com>
2025-02-26 11:07:03 -0700

    Removing the duplicate code that appeared after the merge

--- PS-CODE.py
+++ PS-CODE.py
@@ -34,26 +34,6 @@ print('*' * 50)
 print("{:>38}".format("Step 2: Select a show time"))
 print('*' * 50)
 
-print("1. Avengers: Endgame($12.00)\n2.The Kitchen($10.00)\n3.Killers of the Flower Moon($8.00)\n4.Dune:Part Two($9.50)")
-
-movie = input("Your selection (1-4):")
-match movie:
-    case "1":
-        print("You selected 'Avengers:Endgame' priced at $12.00")
-    case "2":
-        print("You selected ' The Kitchen' priced at $10.00")
-    case "3":
-        print("You selected 'Killers of the Flower Moon ' priced at $8.00")
-    case "4":
-        print("You selected ' Dune: Part Two' priced at $9.50")
-    case _:
-        print('Invalid Selection. Please choose a valid movie number.')
-
-
-
-
-
-
 #Implement show time selection
 print("1. 1:00 PM\n2. 4:30 PM\n3. 8:00 PM") 
 show = input("Your selection (1-3): ")
